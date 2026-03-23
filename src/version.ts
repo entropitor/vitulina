27cf3340ecdef6c2aa0e7fd62a5205e7970ca04d
Jens Claes <896093+entropitor@users.noreply.github.com>
2026-03-23 10:03:30 +0100

    chore: add release workflow

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,3 @@
-import { readFileSync } from "node:fs";
-import { fileURLToPath } from "node:url";
+import pkg from "../package.json" with { type: "json" };
 
-const pkgPath = fileURLToPath(new URL("../package.json", import.meta.url));
-const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
 export const VERSION: string = pkg.version;
